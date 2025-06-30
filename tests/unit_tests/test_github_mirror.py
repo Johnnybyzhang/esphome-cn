8f3e04905bda6210d410078eec3c64f38c854239
Boyan Zhang <42496003+Johnnybyzhang@users.noreply.github.com>
2025-07-01 00:32:13 +0800

    patch submodule git urls

--- tests/unit_tests/test_github_mirror.py
+++ tests/unit_tests/test_github_mirror.py
@@ -1,5 +1,5 @@
 import os
-from esphome.github_mirror import _transform_url, CUSTOM_URL
+from esphome.github_mirror import _transform_url, CUSTOM_URL, _patch_popen
 
 
 def test_transform_url_git_plus():
@@ -11,3 +11,15 @@ def test_transform_url_git_plus():
 def test_transform_url_already_patched():
     url = f"git+{CUSTOM_URL}/https://github.com/example/repo.git"
     assert _transform_url(url) == url
+
+
+def test_patch_popen_list():
+    args = ["git", "clone", "https://github.com/example/repo.git"]
+    result = _patch_popen(args)
+    assert result["args"][2] == f"{CUSTOM_URL}/https://github.com/example/repo.git"
+
+
+def test_patch_popen_string():
+    cmd = "git clone https://github.com/example/repo.git"
+    result = _patch_popen(cmd)
+    assert f"{CUSTOM_URL}/https://github.com/example/repo.git" in result["args"]
